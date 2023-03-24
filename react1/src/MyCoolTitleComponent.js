2a63b67b37a310959f1811f86e425529db976167
kaaarma <karukati@gmail.com>
2023-03-24 10:22:46 +0200

    Author and musician, using React with API

---
+++ react1/src/MyCoolTitleComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css"
+
+
+function MyCoolTitleComponent() {
+
+    return (
+        <div>
+            <h2>Hello, xxx xxx</h2>
+            <p>This is a first paragraph.</p>
+        </div>
+    );
+
+}
+
+export default MyCoolTitleComponent;
