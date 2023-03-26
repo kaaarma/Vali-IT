3d01f05c569763476cb92bfc7d09f9f3ddde6443
kaaarma <karukati@gmail.com>
2023-03-26 17:40:29 +0300

    react

--- ToHourMin.js
+++
@@ -1,8 +0,0 @@
-
-function convert() {
-    let userNr = document.getElementById("userNr").value;
-    let hours = Math.floor(userNr / 60);
-    let min = userNr - (hours * 60);
-    document.getElementById("result").innerHTML = String(hours) + " hours and " + String(min) + " minutes";
-
-}
